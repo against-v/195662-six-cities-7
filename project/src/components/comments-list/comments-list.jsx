0a0d7c40a4174ad4eb2b6968c8cbb2b8a8896d23
against-v <against.vegetables@gmail.com>
2021-07-17 00:21:16 +0300

    module7-task1: - подключение к хранилищу через хуки

--- project/src/components/comments-list/comments-list.jsx
+++ project/src/components/comments-list/comments-list.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 import commentProp from '../comment/comment.prop';
 
 import CommentForm from '../comment-form/comment-form';
 import Comment from '../comment/comment';
-import {AuthorizationStatus} from '../../const';
-import {getAuthorizationStatus} from '../../store/user/selectors';
+import {getIsAuthorized} from '../../store/user/selectors';
 
 function CommentsList(props) {
   const {
     comments,
     commentsCount,
-    isAuthorized,
   } = props;
+
+  const isAuthorized = useSelector(getIsAuthorized);
+
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{commentsCount}</span></h2>
@@ -33,16 +34,10 @@ function CommentsList(props) {
   );
 }
 
-const mapStateToProps = (state) => ({
-  isAuthorized: getAuthorizationStatus(state) === AuthorizationStatus.AUTH,
-});
-
 CommentsList.propTypes = {
   comments: PropTypes.arrayOf(commentProp).isRequired,
   commentsCount: PropTypes.number.isRequired,
-  isAuthorized: PropTypes.bool.isRequired,
 };
 
-export {CommentsList};
-export default connect(mapStateToProps)(CommentsList);
+export default CommentsList;
 
