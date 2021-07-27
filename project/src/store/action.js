b580eae31f7ec1b6efa9c539b3cc1b691ecdce3f
against-v <against.vegetables@gmail.com>
2021-07-27 13:18:07 +0300

    module7-task1: - action

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -16,42 +16,16 @@ export const ActionType = {
   SET_COMMENT_FORM_ERROR: 'setCommentFormError',
 };
 
-export const setCity = createAction(ActionType.SET_CITY, (payload) => ({
-  payload,
-}));
-export const setSortType = createAction(ActionType.SET_SORT_TYPE, (payload) => ({
-  payload,
-}));
-export const setActiveCardId = createAction(ActionType.SET_ACTIVE_CARD_ID, (payload) => ({
-  payload,
-}));
-export const loadOffers = createAction(ActionType.LOAD_OFFERS, (payload) => ({
-  payload,
-}));
-export const loadOffer = createAction(ActionType.LOAD_OFFER, (payload) => ({
-  payload,
-}));
-export const loadComments = createAction(ActionType.LOAD_COMMENTS, (payload) => ({
-  payload,
-}));
-export const resetOffer = createAction(ActionType.RESET_OFFER, (payload) => ({
-  payload,
-}));
-export const requireAuthorization = createAction(ActionType.REQUIRE_AUTHORIZATION, (payload) => ({
-  payload,
-}));
-export const setUser = createAction(ActionType.SET_USER, (payload) => ({
-  payload,
-}));
-export const logout = createAction(ActionType.LOGOUT, (payload) => ({
-  payload,
-}));
-export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE, (payload) => ({
-  payload,
-}));
-export const setCommentFormIsLoading = createAction(ActionType.SET_COMMENT_FORM_IS_LOADING, (payload) => ({
-  payload,
-}));
-export const setCommentFormError = createAction(ActionType.SET_COMMENT_FORM_ERROR, (payload) => ({
-  payload,
-}));
+export const setCity = createAction(ActionType.SET_CITY);
+export const setSortType = createAction(ActionType.SET_SORT_TYPE);
+export const setActiveCardId = createAction(ActionType.SET_ACTIVE_CARD_ID);
+export const loadOffers = createAction(ActionType.LOAD_OFFERS);
+export const loadOffer = createAction(ActionType.LOAD_OFFER);
+export const loadComments = createAction(ActionType.LOAD_COMMENTS);
+export const resetOffer = createAction(ActionType.RESET_OFFER);
+export const requireAuthorization = createAction(ActionType.REQUIRE_AUTHORIZATION);
+export const setUser = createAction(ActionType.SET_USER);
+export const logout = createAction(ActionType.LOGOUT);
+export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE);
+export const setCommentFormIsLoading = createAction(ActionType.SET_COMMENT_FORM_IS_LOADING);
+export const setCommentFormError = createAction(ActionType.SET_COMMENT_FORM_ERROR);
